93f9ffd82509faf555b9f77c6ff6170d1208e282
hartator <hartator@gmail.com>
2018-08-10 16:44:24 -0500

    Test related questions "People also ask" block

---
+++ README.md
@@ -0,0 +1,5 @@
+# Organic Results Desktop
+
+[![Build Status](https://travis-ci.org/serpapi/test-organic-results-desktop.svg?branch=master)](https://travis-ci.org/serpapi/test-organic-results-desktop)
+
+Tests for SerpApi desktop organic results
---
+++ spec/related_questions_coffee_spec.rb
@@ -0,0 +1,40 @@
+describe "SerpApi Desktop JSON" do
+
+  describe "Related Questions for Coffee" do
+
+    before :all do
+      @response = HTTP.get 'https://serpapi.com/search.json?q=Coffee&location=Dallas&hl=en&gl=us&source=test'
+      @json = @response.parse
+    end
+
+    it "returns http success" do
+      expect(@response.code).to be(200)
+    end
+
+    it "contains organic results array" do
+      expect(@json["related_questions"]).to be_an(Array)
+    end
+
+    describe "have \"Is coffee really good for your health?\" question" do
+
+      before :all do
+        @question = @json["related_questions"].detect{|e| e["question"] == "Is coffee really good for your health?"}
+      end
+
+      it "has title from Time" do
+        expect(@question["title"]).to eql("Coffee and health: What does the research say? - Mayo Clinic")
+      end
+
+      it "links Time" do
+        expect(@question["link"]).to eql("https://www.mayoclinic.org/healthy-lifestyle/nutrition-and-healthy-eating/expert-answers/coffee-and-health/faq-20058339")
+      end
+
+      it "has a snippet" do 
+        expect(@question["snippet"]).to_not be_empty
+      end
+
+    end
+
+  end
+
+end
