6411da22570520edb74d324523788a2ff2cf36c4
hartator <hartator@gmail.com>
2018-08-14 15:04:09 -0500

    Test proper emoji support inside answers

---
+++ spec/related_questions_bitcoin_spec.rb
@@ -0,0 +1,44 @@
+describe "SerpApi Desktop JSON" do
+
+  describe "Related Questions for Bitcoin (emoji 👏 issue)" do
+
+    before :all do
+      @response = HTTP.get 'http://localhost:3000/search.json?q=How+do+you+get+bitcoins%3F&location=Dallas&hl=en&gl=us&source=test'
+      @json = @response.parse
+    end
+
+    it "returns http success" do
+      expect(@response.code).to be(200)
+    end
+
+    it "contains organic results array" do
+      expect(@json["related_questions"]).to be_an(Array)
+    end
+
+    describe "have \"How do you earn bitcoins?\" question" do
+
+      before :all do
+        @question = @json["related_questions"].detect{|e| e["question"] == "How do you earn bitcoins?"}
+      end
+
+      it "has title from Time" do
+        expect(@question["title"]).to eql("Earn Bitcoins in 8 different ways")
+      end
+
+      it "links Time" do
+        expect(@question["link"]).to eql("http://earn-bitcoins.com/")
+      end
+
+      it "has a snippet" do 
+        expect(@question["snippet"]).to_not be_empty
+      end
+
+      it "includes 👏 emoji" do 
+        expect(@question["snippet"]).to include("👏")
+      end
+
+    end
+
+  end
+
+end
